6c845fa3c115d2dc895f0d6ddbe85ca13e6465d6
Thompson Dean <dean.thompson@arsaga.jp>
2022-10-18 14:51:58 +0900

    Initial Commit

---
+++ NaturalLanguageTutorial/NaturalLanguageTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaturalLanguageTutorialApp.swift
+//  NaturalLanguageTutorial
+//
+//  Created by Thompson Dean on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct NaturalLanguageTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
